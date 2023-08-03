ce83a70ca775e3733ce3bb79b1626f0f667c45be
Ryan Seipp <rseipp@ryanseipp.com>
2023-08-03 01:44:35 -0400

    Reallow used lints

--- src/rask-liburing-sys/src/lib.rs
+++ src/rask-liburing-sys/src/lib.rs
@@ -7,6 +7,9 @@
 )]
 // Disallow warnings in examples.
 #![doc(test(attr(deny(warnings))))]
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
 
 //! Rust binding for liburing
 
