05a854198d23b662e9df68f1fd5420fc13ed2f91
Ryan Seipp <rseipp@ryanseipp.com>
2023-08-03 01:43:07 -0400

    Remove unused allowed lints

--- src/rask-liburing-sys/src/lib.rs
+++ src/rask-liburing-sys/src/lib.rs
@@ -7,9 +7,6 @@
 )]
 // Disallow warnings in examples.
 #![doc(test(attr(deny(warnings))))]
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
 
 //! Rust binding for liburing
 
